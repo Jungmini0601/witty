2f9180f46f146199d71a6bd4372a82b0449043c5
seongmin2001 <os20012013@gmail.com>
2022-01-28 23:16:30 +0900

    임시 저장

--- src/main/java/com/daelim/witty/web/service/UserService.java
+++ src/main/java/com/daelim/witty/web/service/UserService.java
@@ -2,13 +2,13 @@ package com.daelim.witty.web.service;
 
 import com.daelim.witty.domain.User;
 import com.daelim.witty.web.controller.dto.UserLogInDTO;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface UserService {
     /**
      * 회원가입
      * */
     public boolean signUp(User user);
-
     /**
      *  로그인
      * */
