d57d6fce97a1a21fad2305acd951fa0e77c411ab
김진솔 <skek298@gmail.com>
2022-02-17 16:51:29 +0900

    댓글 생성 서비스 추가

--- src/main/java/com/daelim/witty/web/service/comments/CommentServiceImpl.java
+++ src/main/java/com/daelim/witty/web/service/comments/CommentServiceImpl.java
@@ -2,11 +2,16 @@ package com.daelim.witty.web.service.comments;
 
 import com.daelim.witty.domain.Comment;
 import com.daelim.witty.domain.User;
+import com.daelim.witty.domain.Witty;
 import com.daelim.witty.web.controller.dto.comments.CommentUpdateDTO;
 import com.daelim.witty.web.repository.comments.CommentRepository;
+import com.daelim.witty.web.repository.users.UserRepository;
 import com.daelim.witty.web.repository.wittys.WittyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +19,7 @@ public class CommentServiceImpl implements CommentService{
 
     private final CommentRepository commentRepository;
     private final WittyRepository wittyRepository;
+    private final UserRepository userRepository;
 
     /**
      * 진솔이형
@@ -22,7 +28,10 @@ public class CommentServiceImpl implements CommentService{
      * */
     @Override
     public Comment save(Comment comment, Integer wittyId, User user) throws Exception {
-        return null;
+        //User에 userId값 가져오기
+        Witty findWitty = wittyRepository.findById(wittyId).get();
+        //commentRepository에서 save 호출 후 반환하기
+        return commentRepository.save(comment,findWitty,user);
     }
 
     /**
