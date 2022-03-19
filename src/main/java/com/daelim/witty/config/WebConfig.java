f88e9903109c66fa872ea9c42c86ccd4dc5add00
김정민 <jungmini0601@gmail.com>
2022-03-19 18:48:47 +0900

    CORS METHOD 이슈 해결

--- src/main/java/com/daelim/witty/config/WebConfig.java
+++ src/main/java/com/daelim/witty/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.daelim.witty.config;
 import com.daelim.witty.v2.web.argumentResolver.LoginMemberArgumentResolver;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,6 +20,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowCredentials(true);
+        registry.addMapping("/**")
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE")
+                .allowedOrigins("http://localhost:3000").allowCredentials(true);
     }
 }
